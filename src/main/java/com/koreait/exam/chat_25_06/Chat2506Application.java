f416bf40a5e67f2fb4f1a667b4eb145f38523af5
yunzivv <yunzivv@gmail.com>
2025-06-11 13:52:09 +0900

    프로젝트 세팅

---
+++ src/main/java/com/koreait/exam/chat_25_06/Chat2506Application.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.chat_25_06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chat2506Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chat2506Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/exam/chat_25_06/Chat2506ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.chat_25_06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chat2506ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
