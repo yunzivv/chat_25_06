301b33d4ad6743945bf28f57e9ae39be5341d699
yunzivv <yunzivv@gmail.com>
2025-06-12 16:39:04 +0900

    message filtering

--- src/main/java/com/koreait/exam/chat_25_06/ChatController.java
+++ src/main/java/com/koreait/exam/chat_25_06/ChatController.java
@@ -1,26 +1,33 @@
 package com.koreait.exam.chat_25_06;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/chat")
+@Slf4j
 public class ChatController {
-    
-    public List<ChatMessage> chatMessages = new ArrayList<ChatMessage>();
 
-    public record writeChatMessageResponse(long id, String authorName, String content) { }
-    public record writeChatMessageRequest(String authorName, String content) { }
+    private List<ChatMessage> chatMessages = new ArrayList<>();
 
+
+    public record writeChatMessageRequest(String authorName, String content) {
+    }
+
+    public record writeChatMessageResponse(long id, String authorName, String content) {
+
+    }
 
     @PostMapping("/writeMessage")
     @ResponseBody
     public RsData<writeChatMessageResponse> writeMessage(@RequestBody writeChatMessageRequest req) {
-
         ChatMessage message = new ChatMessage(req.authorName, req.content);
+
         chatMessages.add(message);
 
         return new RsData<>(
@@ -30,17 +37,42 @@ public class ChatController {
         );
     }
 
-    public record messagesResponse(List<ChatMessage> messages, long count) { }
+    public record messagesRequest(Long fromId) {
+
+    }
+
+    public record messagesResponse(List<ChatMessage> chatMessages, long count) {
+
+    }
 
     @GetMapping("/messages")
     @ResponseBody
-    public RsData<messagesResponse> showMessages() {
+    public RsData<messagesResponse> showMessages(messagesRequest req) {
+
+        List<ChatMessage> messages = chatMessages;
+
+        log.debug("req : {}", req);
+
+        // 번호가 같이 입력되는 경우
+        if (req.fromId != null) {
+
+            // 해당 번호의 채팅 메세지가 전체 리스트의 몇번째 인덱스인지?
+            int index = IntStream.range(0, messages.size())
+                    .filter(i -> chatMessages.get(i).getId() == req.fromId)
+                    .findFirst()
+                    .orElse(-1);
+
+            if (index != -1) {
+                // 만약에 index가 -1 이 아니라면? 0번부터 index번 까지 제거한 리스트를 만든다.
+                messages = messages.subList(index + 1, messages.size());
+            }
+        }
+
 
         return new RsData<>(
                 "S-1",
                 "성공",
-                new messagesResponse(chatMessages, chatMessages.size())
+                new messagesResponse(messages, messages.size())
         );
     }
 }
-
